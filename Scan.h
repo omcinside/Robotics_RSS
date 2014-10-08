c39736f3f3c11b56c7b69d6536d414b61cc379f2
ameijer <aartmeijer@hotmail.com>
2014-10-08 13:44:17 +0000

    Scan.h edited online with Bitbucket

--- Scan.h
+++ Scan.h
@@ -35,23 +35,29 @@ class Scan {
   }
 
 
-  void scan180(){
+  void scan180(){ // 3 camera shots, 37 IR points
     servo.disconnect();
     servo.engage();
     while(servo.getPosition()<220) { //puts the servo at 0
       servo.lookLeft();
       wait(50); //50ms wait for accuracy
     }
-    wait(500);
+    wait(1000);
     //do some scans
 
-    for(int i=0;i<12;i++) {
-      while(servo.getPosition()<15*(i+1)) {
+    for(int i=0;i<37;i++) {
+      while(servo.getPosition()<5*(i+1)) {
 	servo.lookRight();
+	
 	wait(50);
+	if(servo.getPosition()%60 == 0) {
+	  wait(1000);
+	  //take camera-shot logic
+	}
       }
-      wait(500);
-      //do some scans
+
+      wait(100);
+      //IR-logic
     }
 
     servo.disconnect(); //or servo.disengage() ?
@@ -61,13 +67,31 @@ class Scan {
     scan180();
     Moto.turnRight(5000);
     scan180();
+    Mot.turnRight(5000);
 
     //if camera detects base, update baseSeen to true
   }
 
+  int lookLeft() {
+    servo.lookleft();
+    wait(500);
+    //return IR-distance
+  }
+
+  int lookRight() {
+    while(servo.getPosition<185) {
+      servo.lookright();
+      wait(50);
+    }
+    //return IR-distance
+
+
   int room() {
     //room detection logic here, returns room number here
   }
+
+  void align(int direction) { // align: 0 = align with right wall, 1 = align with left wall
+  } 
 
   int nearestAnomaly() {
     //IR-logic: finds nearest anomaly, returns 0 for N-E, 1 for N-W, 2 for S-E, 3 for S-W, 4 for direct north, 5 for direct south, 6 for direct east, 7 for direct west
